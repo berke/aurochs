7e420e100407826b734ac499e153995d5d29f86c
Berke Durak <berke.durak@exalead.com>
2007-10-02 13:48:13 +0200

    +Stringifier

---
+++ backends/stringifier.ml
@@ -0,0 +1,31 @@
+(* Stringifier *)
+
+open Pffpsf;;
+
+let print_indent oc n =
+  for i = 0 to n - 1 do
+    output_char oc ' '
+  done
+;;
+
+let print_ocaml_string ?(indent=4) ?(cols=32) () oc u =
+  let m = String.length u in
+  print_indent oc (indent - 1);
+  fp oc "\"";
+  let rec loop i =
+    if i = m then
+      fp oc "\"\n"
+    else
+      begin
+        if i > 0 && i mod cols = 0 then
+          begin
+            fp oc "\\\n";
+            print_indent oc indent;
+          end;
+        let c = Char.code u.[i] in
+        fp oc "\\%03d" c;
+        loop (i + 1)
+      end
+  in
+  loop 0
+;;
